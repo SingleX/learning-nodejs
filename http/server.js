7734058eba36a9bbd33326f45f6ebb5f2fdd3160
SingleX <singlexer@gmail.com>
2014-10-22 18:10:21 +0800

    add module

--- http/server.js
+++ http/server.js
@@ -1,12 +1,14 @@
 var http = require('http');
 
-function handle(request, response){
-    console.log('request received...');
-    response.writeHead(200, {'Content-Type': 'text-plain'});
-    response.write('Hello, here is a HTTP server : http://localhost:8888');
-    response.end();
+function start(){
+    function onRequest(request, response){
+        console.log('request received...');
+        response.writeHead(200, {'Content-Type': 'text-plain'});
+        response.write('Hello, here is a HTTP server : http://localhost:8888');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('Server started !');
 }
-http.createServer(handle).listen(8888);
 
-console.log('Server start !');
-
+exports.start = start;
