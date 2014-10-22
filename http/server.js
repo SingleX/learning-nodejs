cf03bcdc98082efe8da46faf25a1a3539146ee93
SingleX <singlexer@gmail.com>
2014-10-22 17:31:30 +0800

    回调函数

--- http/server.js
+++ http/server.js
@@ -1,6 +1,12 @@
 var http = require('http');
-http.createServer(function(request, response){
+
+function handle(request, response){
+    console.log('request received...');
     response.writeHead(200, {'Content-Type': 'text-plain'});
     response.write('Hello, here is a HTTP server : http://localhost:8888');
     response.end();
-}).listen(8888);
+}
+http.createServer(handle).listen(8888);
+
+console.log('Server start !');
+
