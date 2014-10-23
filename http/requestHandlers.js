f20f22331c2013027785cb9eadbde3afd47015c7
SingleX <singlexer@gmail.com>
2014-10-23 18:40:31 +0800

    增加阻塞代码

--- http/requestHandlers.js
+++ http/requestHandlers.js
@@ -1,9 +1,17 @@
 function start(){
     console.log('RequestHandler "start" was called');
+    // 模拟阻塞操作
+    function sleep(milliSeconds){
+        var startTime = new Date().getTime();
+        while(new Date().getTime() < startTime + milliSeconds);
+    }
+    sleep(10000);
+    return "hellp start...";
 }
 
 function upload(){
     console.log('RequestHandler "upload" was called');
+    return "hellp upload...";
 }
 
 exports.start = start;
--- http/router.js
+++ http/router.js
@@ -2,9 +2,10 @@ function route(handle, pathname){
     console.log("About to route request : " + pathname);
 
     if(typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        return handle[pathname]();
     }else{
         console.log("No route handler found for " + pathname);
+        return "404 Not Found !";
     }
 }
 
